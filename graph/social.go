796771fdc35382422884edca4c482671d7a0f90f
1nthe4byss <christian4onos@outlook.com>
2025-10-31 21:29:02 +0100

    31 oct: everywhere semo (cycle importation fixed)

--- graph/social.go
+++ graph/social.go
@@ -1,13 +1,21 @@
 package graph
 
-type NodeID string
-type RelationshipType string
+import (
+	"findX/model"
+)
 
 const (
-	Followers RelationshipType = "followers"
-	Following RelationshipType = "following"
-	Mentions  RelationshipType = "mentions"
-	Likes     RelationshipType = "likes"
-	Comments  RelationshipType = "comments"
-	Tagged    RelationshipType = "tagged"
+	Followers model.RelationshipType = "followers"
+	Following model.RelationshipType = "following"
+	Mentions  model.RelationshipType = "mentions"
+	Likes     model.RelationshipType = "likes"
+	Comments  model.RelationshipType = "comments"
+	Tagged    model.RelationshipType = "tagged"
 )
+
+func NewSocialGraph() *model.SocialGraph {
+	return &model.SocialGraph{
+		Nodes: make(map[model.NodeID]*model.User),
+		Edges: make(map[model.NodeID]map[model.NodeID]*model.Relationship),
+	}
+}
--- model/Relationship.go
+++ model/Relationship.go
@@ -1,12 +1,21 @@
 package model
 
 import (
-	"findX/graph"
+	"sync"
 )
 
+type NodeID string
+type RelationshipType string
+
 type Relationship struct {
-	From   graph.NodeID
-	To     graph.NodeID
-	Type   graph.RelationshipType
+	From   NodeID
+	To     NodeID
+	Type   RelationshipType
 	Weight int // optional: how strong or frequent the connection is
+}
+
+type SocialGraph struct {
+	Nodes map[NodeID]*User
+	Edges map[NodeID]map[NodeID]*Relationship
+	mu    sync.RWMutex
 }
