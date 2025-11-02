cb5326ad6724a7d74eaa3a79bb0c7e3041d36b59
1nthe4byss <christian4onos@outlook.com>
2025-11-03 00:56:26 +0100

    3rd Nov: testing usablility

---
+++ scrape/gen.go
@@ -0,0 +1,23 @@
+package scrape
+
+import (
+	"context"
+	"findX/graph"
+	"log"
+
+	"github.com/chromedp/chromedp"
+)
+
+func x_scrape() {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	g := graph.NewSocialGraph()
+	scraper := NewScraper(ctx, "")
+
+	if err := scraper.GetUserData("nasa", g); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("📊 Total users: %d", len(g.GetAllUsers()))
+}
